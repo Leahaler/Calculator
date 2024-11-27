9bfbff9075eac64a3aee1a27ad149279ca509d06
Ксеша <leafeon120@gmail.com>
2024-11-27 22:00:57 +0300

    Update calc.py

--- calc.py
+++ calc.py
@@ -11,54 +11,46 @@ symbols = {'ноль' : 0, 'один' : 1, 'два' : 2, 'три' : 3, 'четы
            'девятьсот': 900, 'одна тысяча': 1000, 'две тысячи': 2000,
            'три тысячи': 3000,'четыре тысячи': 4000, 'пять тысяч': 5000,
            'шесть тысяч': 6000, 'семь тысяч': 7000,'восемь тысяч': 8000,
-           'девять тысяч': 9000,'плюс' : '+', 'минус' : '-', 'умножить' : '*' }
+           'девять тысяч': 9000,'плюс' : '+', 'минус' : '-', 'умножить' : '*',
+           'открывается': '(', 'закрывается': ')'}
 
 def key_value(symbol):
+    '''
+    функция принимает число(цифры) и возвращает его письменную запись
+    '''
     for key, value in symbols.items():
         if symbol == value:
             return key
-def calc():
-    
-    example = []
-    string = list(input('Введите пример: ').split())
-    numbers = ['_']
-    op = []
+def calc(string):
+    '''
+    функция принимает строку(пример)преобразовывает ее в числовую запись,
+    после чего выполняет вычисления и выводит результат
+    '''
+    result_0 = []
+    result_1 = ''
+    digit = []
+
     while 'на' in string:
         string.remove('на')
+    while 'скобка' in string:
+        string.remove('скобка')
         
     for symbol in string:
         try:
             int(symbols[symbol])
             try:
-                int(numbers[-1])
-                numbers[-1] += symbols[symbol]
+                int(result_0[-1])
+                result_0[-1] += symbols[symbol]
             except:
-                numbers.append(symbols[symbol])
-                op.append(None)
+                result_0.append(symbols[symbol])
         except:
-            op.append(symbols[symbol])
-            numbers.append(None)
-    numbers.remove('_')
-    
-    print(numbers)
-    print(op)
-    while '*' in op:
-        numbers[op.index('*')-1] = numbers[op.index('*') - 1] * numbers[op.index('*') +1]
-        del numbers[op.index('*')+1]
-        del numbers[op.index('*')]
-        del op[op.index('*') + 1]
-        del op[op.index('*')]
-    result = numbers[0]
-    for indx in range(1, len(numbers)):
-        try:
-            int(numbers[indx])
-            if op[indx - 1] == '+':
-                result += numbers[indx]
-            else:
-                result -= numbers[indx]
-        except:
-            continue
-    digit = []
+            result_0.append(symbols[symbol])
+            
+    for elem in result_0:
+        result_1 += str(elem)
+        
+    result = eval(result_1)
+
     for ten in range(1, len(str(result))+1):
         if result%100 > 10 and result%100 < 20 and ten == 1:
             continue
@@ -66,9 +58,9 @@ def calc():
             if result % (10**ten) != 0:
                 digit.insert(0, result % (10**ten))
                 result -= result % (10**ten)
-    print(digit)
-    print(result)
-
+    print('Ваш результат: ', end ='')            
     for symbol in digit:
         print(key_value(symbol), end =' ')
-calc()
+string = list(input('Введите пример: ').split())
+calc(string)
+
